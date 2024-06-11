d031e11f5fc4b2887ae66fb7874eab106040d7d8
stefano.peraldini <stefano.peraldini@hackaboss.com>
2024-06-11 19:46:55 +0200

    API express básica

--- back/server.js
+++ back/server.js
@@ -1,3 +1,17 @@
 "use strict"
+import dotenv from "dotenv";
+dotenv.config();
 
-console.log("¡Ciao Mondo!")
+import express from "express"
+
+const app = express();
+
+const {PORT} = process.env
+
+app.get("/", (req, res)=> {
+    res.send("¡Hola desde PFB!")
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server on ${PORT}`)
+})
